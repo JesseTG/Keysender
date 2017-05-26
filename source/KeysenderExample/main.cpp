e76f2a6c94a1f36724b2413bc76830d3f99b3045
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2017-05-25 20:38:59 -0400

    Add the ability to list supported key codes to the example app

--- source/KeysenderExample/main.cpp
+++ source/KeysenderExample/main.cpp
@@ -1,24 +1,63 @@
+#include <algorithm>
+#include <cstdio>
+
 #include <QCoreApplication>
 #include <QCommandLineParser>
 #include <QKeySequence>
 #include <QtDebug>
+#include <QDebug>
+#include <QFile>
 #include <QKeyEvent>
 
 #include "Keysender/Keysender.hpp"
+#include "Keysender/Keysender-version.h"
 
 int main(int argc, char *argv[])
 {
   QCoreApplication app(argc, argv);
-  app.setApplicationName("Keysender-Test");
-  app.setApplicationVersion("0.1");
+  app.setApplicationName("KeysenderExample");
+  app.setApplicationVersion(KEYSENDER_VERSION);
   QCommandLineParser parser;
   parser.setApplicationDescription(
-    "TODO"
+    "Send keypresses to the windowing system"
   );
+
+  parser.addOptions({
+    {{"l", "list"}, "Lists the key codes currently available"}
+  });
+  parser.addPositionalArgument("keys", "The names of the keys to send to the windowing system", "<key>...");
   parser.addHelpOption();
   parser.addVersionOption();
 
   parser.process(app);
+
+  QFile output;
+  output.open(stdout, QIODevice::WriteOnly);
+
+  QDebug out(&output);
+  keysender::Keysender keysender;
+
+  if (parser.isSet("list")) {
+    // If we just want to list the key codes...
+    auto keys = keysender.getSupportedKeys();
+
+    if (keys.empty()) {
+      qCritical() << "Couldn't determine which key codes can be used!";
+      return 1;
+    }
+
+    auto keyList = keys.toList();
+    std::sort(keyList.begin(), keyList.end());
+
+    out.nospace();
+    for (Qt::Key key : keyList) {
+      // For each key, in sorted order...
+      out << key << '\n';
+    }
+
+    output.flush();
+    return 0;
+  }
 
 
   QStringList sequences = parser.positionalArguments();
@@ -26,7 +65,6 @@ int main(int argc, char *argv[])
     parser.showHelp(1);
   }
 
-  keysender::Keysender keysender;
 
 
   for (const QString& s : sequences) {
